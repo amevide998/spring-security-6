fd78684d94cbbb079c78eb78bd0ccfa311577920
amevide998 <hdsvidi@gmail.com>
2023-10-21 21:29:34 +0700

    update

--- src/main/java/com/hdscode/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/hdscode/springsecurity/security/ApplicationSecurityConfig.java
@@ -16,8 +16,14 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.hdscode.springsecurity.security.ApplicationUserRoles.*;
 import static com.hdscode.springsecurity.security.ApplicationUserPermission.*;
@@ -36,16 +42,19 @@ public class ApplicationSecurityConfig{
     }
 
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 // spring security 6. manual using tokenrepository and token handler
-                .csrf((csrf) -> csrf
-                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                        .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler())
-                )
+//                .csrf((csrf) -> csrf
+//                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                        .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler())
+//                )
+
+                // disable csrf
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
                         // example to permit access without authentication
-                        .requestMatchers("/index.html", "/","/static/**").permitAll()
+                        .requestMatchers("/index.html", "/login.html","/static/**").permitAll()
 
                         // example to permit student role to access /api/** end point
 //                        .requestMatchers("/api/**").hasRole(STUDENT.name())
@@ -56,10 +65,44 @@ public class ApplicationSecurityConfig{
 //                        .requestMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
 //                        .requestMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                         .anyRequest().authenticated()
+
                 )
-                .httpBasic(Customizer.withDefaults());
+                // form login - dont know why, but it works if httpBasic is disabled
+                // using default redirect after success login
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                        .defaultSuccessUrl("/courses", true)
+                        // define parameter (this is actually default parameter)
+                        .passwordParameter("password")
+                        .usernameParameter("username")
+                )
+
+
+
+//                .httpBasic(Customizer.withDefaults());
+                .rememberMe(rememberMe ->
+                        rememberMe
+                                .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+                                .key("very secured")
+                // remember me paramater
+                                .rememberMeParameter("remember-me")
+                )
+
+                // logout implementation
+                .logout(logout -> logout
+                                .logoutUrl("/logout")
+                        // using logout request mathcher if csrf is disable because logout using GET. if using csrf (default logout will be using POST
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                                .invalidateHttpSession(true)
+                                .clearAuthentication(true)
+                                .deleteCookies("JSESSIONID", "remember-me")
+                                .logoutSuccessUrl("/login")
+                        );
+//                .rememberMe(Customizer.withDefaults());
         return http.build();
     }
+
 
     @Bean
     public UserDetailsService userDetailsService(){
